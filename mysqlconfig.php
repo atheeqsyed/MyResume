60f94c237febb9380702e14e0484ea71bb6c2dc2
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-20 22:07:35 +0530

    Update mysqlconfig.php

--- mysqlconfig.php
+++ mysqlconfig.php
@@ -1,17 +1,14 @@
 <?php
-
+// Mysql config
 class MysqlConfig
 {
-
     public $connection;
-
     public function __construct()
     {
         $host = "localhost";
         $uname = "root";
         $pwd = "A@#345_abcd1";
         $dbname = "Protofolio";
-       
         $this->connection = new mysqli($host, $uname, $pwd, $dbname) or die("Connect failed: %s\n");
     }
 
